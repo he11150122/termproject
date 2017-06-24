789723199d10e42605aab3ec4b279701dba674c1
mac <mac@DESKTOP-4KQOCTL>
2017-06-24 20:55:58 +0900

    0624최종

--- term/src/term/Cam.java
+++ term/src/term/Cam.java
@@ -3,24 +3,24 @@ package term;
 import static com.googlecode.javacv.cpp.opencv_core.cvFlip;
 import static com.googlecode.javacv.cpp.opencv_highgui.cvSaveImage;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import com.googlecode.javacv.CanvasFrame;
 import com.googlecode.javacv.FrameGrabber;
 import com.googlecode.javacv.VideoInputFrameGrabber;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 import javax.swing.*;
 import javax.swing.event.*;
-
 import java.awt.image.RenderedImage;
 import java.io.*;
 import javax.imageio.*;
 public class Cam implements Runnable {
 
+	FaceDetection FD = new FaceDetection();
     IplImage image;
     CanvasFrame canvas = new CanvasFrame("æÛ±º∞À√‚¿ª ¿ß«— √‘øµ");
     public Cam() {
         canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
     }
-    @Override
     public void run() {
         FrameGrabber grabber = new VideoInputFrameGrabber(0);
         try {
@@ -30,18 +30,19 @@ public class Cam implements Runnable {
             	img = grabber.grab();
             	if (img != null) {
             	cvFlip(img, img, 3);
-                cvSaveImage("face.jpg", img);                    
+                cvSaveImage("./bin/term/face.jpg", img);
+                FD.faceDetect();
                 canvas.showImage(img);
                 }
             }               
-        } catch (Exception e) {
-        }
+        } catch (Exception e) {}
 
     }
     public static void main(String[] args) {
         Cam c = new Cam();
         Thread th = new Thread(c);
         th.start();
+        
     }    
     
 }
--- term/src/term/FaceDetection.java
+++ term/src/term/FaceDetection.java
@@ -6,13 +6,16 @@ import javax.swing.*;
 import org.opencv.core.*;
 import org.opencv.core.Point;
 import org.opencv.objdetect.CascadeClassifier;
+
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class FaceDetection{
-	public static void main(String args[]){
+	public void faceDetect(){
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
+		Rect rectCrop;
         CascadeClassifier faceDetector = new CascadeClassifier(FaceDetection.class.getResource("haarcascade_frontalface_alt.xml").getPath().substring(1));
         Mat image = Imgcodecs.imread(FaceDetection.class.getResource("face.jpg").getPath().substring(1));
         MatOfRect faceDetections = new MatOfRect();
@@ -21,10 +24,36 @@ public class FaceDetection{
         System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
  
         for (Rect rect : faceDetections.toArray()) {
+        	//이미지 사각형 표시
         	Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
                     new Scalar(0, 255, 0));
+        	//이미지 크롭
+        	rectCrop = new Rect(rect.x,rect.y,rect.width,rect.height);
+        	//크롭이미지 매트 형태로 저장
+        	Mat imageroi= new Mat(image,rectCrop);
+        	//리사이즈 저장용 매트 선언
+        	Mat resizeImage = new Mat();
+        	//리사이즈용 사이즈객체 선언
+        	Size sz = new Size(1000,1000);
+        	//이미지 리사이즈
+        	Imgproc.resize(imageroi, resizeImage, sz);
+        	//리사이즈된 이미지 저장
+        	Imgcodecs.imwrite("ouput_face.jpg", resizeImage);
         }
+        
+        String filename;
+        //length>=1이 검출된 얼굴이 있음 
+        if(faceDetections.toArray().length >=1)
+        	filename = "ouput.jpg";
+        else
+        	filename = "ouput1.jpg";
+        
+        System.out.println(String.format("Writing %s", filename));
+        Imgcodecs.imwrite(filename, image);
 
-		}
+	}
+
+
+
 }
 
